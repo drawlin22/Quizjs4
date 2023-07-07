cd486ec2e75c1e715c1ebd297c7cb3682408e02f
Donnie Rawlings <tacobellboi@yahoo.com>
2023-07-06 18:23:40 -0700

    initial file set up

---
+++ script.js
@@ -0,0 +1,64 @@
+let questions;
+let score;
+
+questions= [
+    {
+        question: "1. What does API Stand for?"
+        selections: "Application Programming Interface", "Accessory Programming Interface", "Application Programming index", "Accessory Programming Index", "Application Planning Interface"
+    },
+
+    {
+        question: "2. Which of the following is not a primitive data type?"
+        selections: "Boolean", "Object", "String", "Number", "Symbol"
+    },
+
+    {
+        question: "3. Which of the following is not and event in javascript?"
+        selections: "onclick", "Offline", "keydown", "onaround", "onabort"
+    },
+
+    {
+        question: "4. What does HTML stand for?"
+        selections:
+    },
+
+    {
+        question: "5. What does DOM stand for?"
+        selections: "Document Object Model"
+    },
+
+    {
+        question: "6."
+        selections:
+    },
+
+    {
+        question: "7."
+        selections:
+    },
+
+    {
+        question: "8."
+        selections:
+    },
+
+    {
+        question: "9."
+        selections:
+    },
+
+    {
+        question: "10."
+        selections:
+    },
+
+    
+
+]
+
+
+const quiz = document.querySelector("quiz")
+
+questions.array.forEach(question, index => {
+    
+});
